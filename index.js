a415c1bfab9416d79dd6d32e401f6306bbb22271
Patrick Fortaleza <patrickfortaleza@gmail.com>
2020-10-08 09:03:42 -0700

    printSelerction function

--- index.js
+++ index.js
@@ -190,7 +190,7 @@ renderTables(desserts, dessertTable);
 // ----------------//
 renderTables(beverages, beverageTable);
 
-
+// Renders table rows
 function renderTables(array, table) {
   for(i = 0; i < array.length; i++){
     // create a table row element 
@@ -217,62 +217,35 @@ function renderTables(array, table) {
 // ----------------//
 // 5.1 Appetizers
 // ----------------//
-
-for(i = 0; i < appetizers.length; i++){
-  // create an option element 
-  const option = document.createElement('option');
-  // edit the inner HTML to include the price and name of item
-  option.innerHTML = `${appetizers[i].name}  $${appetizers[i].price}`
-  // add attribute of value and assign the item's ID
-  option.setAttribute("value", appetizers[i].id);
-  // then we select the table, and append the table row which now includes the two <td>'s
-  appetizerSelect.appendChild(option);
-}
-
+renderSelect(appetizers, appetizerSelect);
 
 // ----------------//
 // 5.2 maincourses
 // ----------------//
-
-for(i = 0; i < maincourses.length; i++){
-  // create an option element 
-  const option = document.createElement('option');
-  // edit the inner HTML to include the price and name of item
-  option.innerHTML = `${maincourses[i].name} $${maincourses[i].price}`
-  // add attribute of value and assign the item's ID
-  option.setAttribute("value", maincourses[i].id);
-  // then we select the table, and append the table row which now includes the two <td>'s
-  mainCourseSelect.appendChild(option); 
-}
+renderSelect(maincourses, maincourseSelect);
 
 // ----------------//
 // 5.3 Desserts
 // ----------------//
-
-for(i = 0; i < desserts.length; i++) {
-  // create an option element 
-  const option = document.createElement('option');
-  // edit the inner HTML to include the price and name of item
-  option.innerHTML = `${desserts[i].name} $${desserts[i].price}`
-  // add attribute of value and assign the item's ID
-  option.setAttribute("value", desserts[i].id);
-  // then we select the table, and append the table row which now includes the two <td>'s
-  dessertSelect.appendChild(option); 
-}
+renderSelect(desserts, dessertSelect);
 
 // ----------------//
 // 5.4 Beverages
 // ----------------//
+renderSelect(beverages, beverageSelect);
 
-for(i = 0; i < beverages.length; i++){
-  // create an option element 
-  const option = document.createElement('option');
-  // edit the inner HTML to include the price and name of item
-  option.innerHTML = `${beverages[i].name} $${beverages[i].price}`
-  // add attribute of value and assign the item's ID
-  option.setAttribute("value", beverages[i].id);
-  // then we select the table, and append the table row which now includes the two <td>'s
-  beverageSelect.appendChild(option); 
+// Renders select options
+function renderSelect(array, select){
+  for(i = 0; i < array.length; i++){
+    // create an option element 
+    const option = document.createElement('option');
+    // edit the inner HTML to include the price and name of item
+    option.innerHTML = `${array[i].name}  $${array[i].price}`
+    // add attribute of value and assign the item's ID
+    option.setAttribute("value", array[i].id);
+    // then we select the table, and append the table row which now includes the two <td>'s
+    select.appendChild(option);
+  }
 }
 
 // ---------------------------------------------- //
@@ -294,6 +267,7 @@ const totalOutput = output.querySelector('#totalOutput')
 
 // Initialize Empty Array
 var orders = []
+var orderPrices = []
 
 // query submit button
 const button = document.querySelector('#submit');
@@ -305,48 +279,24 @@ button.addEventListener("click", function(e){
   // ----------------//
   // 6.2 Appetizers
   // ----------------//
-
-  // Pull value of appetizer select
-  var appetizerSelectedId = appetizerSelect.value;
-  // Find the selected appetizer in appetizer menu
-  const appetizerResult = appetizers.filter(appetizer => appetizer.id == appetizerSelectedId );
-  // push selected appetizer to orders
-  orders.push(appetizerResult[0].name);
-  appetizerOutput.innerHTML = appetizerResult[0].name
+  printSelection(appetizers, appetizerSelect, appetizerOutput);
     
   // ----------------//
   // 6.3 maincourses
   // ----------------//
-
-  // Pull value of maincourse select
-  var maincourseSelectedId = maincourseSelect.value;
-  // Find the selected appetizer in appetizer menu
-  const maincourseResult = maincourses.filter(maincourse => maincourse.id == maincourseSelectedId );
-  // push selected appetizer to orders
-  maincourseOutput.innerHTML = maincourseResult[0].name
-
+  printSelection(maincourses, maincourseSelect, maincourseOutput);
   
   // ----------------//
   // 6.4 Deserts
   // ----------------//
-
-  // Pull value of dessert select
-  var DessertSelectedId = dessertSelect.value;
-  // Find the selected appetizer in appetizer menu
-  const DessertResult = desserts.filter(DesertMenu => DesertMenu.id == DessertSelectedId );
-  // push selected appetizer to orders
-  dessertOutput.innerHTML = DessertResult[0].name
+  printSelection(desserts, dessertSelect, dessertOutput);
 
     
   // ----------------//
   // 6.5 Beverages
   // ----------------//
+  printSelection(beverages, beverageSelect, beverageOutput);
 
-  // Pull value of beverage select
-  var beverageSelectedId = beverageSelect.value;
-  // Find the selected appetizer in appetizer menu
-  const beverageResult = beverages.filter(beverageMenu => beverageMenu.id == beverageSelectedId );
-  beverageOutput.innerHTML = beverageResult[0].name;
 
   // ----------------//
   // 6.7 Compile
@@ -354,19 +304,28 @@ button.addEventListener("click", function(e){
 
   var price;
 
-  price = beverageResult[0].price + 
-          DessertResult[0].price + 
-          maincourseResult[0].price + 
-          appetizerResult[0].price;
+  price = orderPrices.reduce((a, b) => a + b, 0);
 
-  console.log(`total Bill:$ ${price.toFixed(2)}`)
+  // console.log(`total Bill:$ ${price.toFixed(2)}`)
   console.log(orders);
   console.log(price);
 
-  totalOutput.innerHTML = `${price.toString()}`;
+  totalOutput.innerHTML = `${price.toFixed(2)}`;
 
 
 });
+
+
+function printSelection(array, select, output){
+  // Pull value of beverage select
+  var selectedId = select.value;
+  // Find the selected appetizer in appetizer menu
+  const result = array.filter(a => a.id == selectedId );
+  output.innerHTML = result[0].name;
+  // Push orders to respective array
+  orders.push(result[0].name)
+  orderPrices.push(result[0].price);
+}
 
 // ---------------------------------------------- //
 // 7. Clear order
@@ -380,13 +339,19 @@ Clear.addEventListener("click", function(e){
   // prevent default form behaviour
   e.preventDefault();
   // reset select boxes
-  beverageSelect.value=1;
-  dessertSelect.value=1;
-  maincourseSelect.value=1;
-  appetizerSelect.value=1;
+  beverageSelect.value = 1;
+  dessertSelect.value = 1;
+  maincourseSelect.value = 1;
+  appetizerSelect.value = 1;
+  // reset output
+  appetizerOutput.innerHTML = null 
+  maincourseOutput.innerHTML = null 
+  dessertOutput.innerHTML = null 
+  beverageOutput.innerHTML = null 
+  totalOutput.innerHTML = null 
   //reset menuArray and price
   var orders = []
-  console.log(orders);
+  var orderPrices = []
 });
 
 
